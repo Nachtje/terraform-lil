299ee6d00e98ba190115e6a366a31cee154063fd
Nathalie Nachtergaele <nathalie.nachtergaele@tomtom.com>
2018-09-13 14:29:02 +0200

    updating availability zone

--- resources.tf
+++ resources.tf
@@ -9,6 +9,6 @@ resource "aws_vpc" "environment-example-two" {
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.environment-example-two.id}"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 }
 
