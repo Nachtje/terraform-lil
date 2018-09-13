eb2d8dbfa5def085b3b3b1efce0070f9c863fa72
Nathalie Nachtergaele <nathalie.nachtergaele@tomtom.com>
2018-09-13 15:51:48 +0200

    vpc with 2 subnets

--- resources.tf
+++ resources.tf
@@ -6,13 +6,13 @@ resource "aws_vpc" "environment-example-two" {
     Name = "terraform-aws-vpc-example-two"
   }
 }
-#resource "aws_subnet" "subnet1" {
-#  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-#  vpc_id = "${aws_vpc.environment-example-two.id}"
-#  availability_zone = "us-east-1a"
-#}
-#resource "aws_subnet" "subnet2" {
-#  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
-#  vpc_id = "${aws_vpc.environment-example-two.id}"
-#  availability_zone = "us-east-2b"
-#}
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-2b"
+}
