51b7be861fe34e2304717be2fe082ce4e4860b54
Nathalie Nachtergaele <nathalie.nachtergaele@tomtom.com>
2018-09-13 15:44:54 +0200

    added another subnet

--- resources.tf
+++ resources.tf
@@ -11,4 +11,8 @@ resource "aws_subnet" "subnet1" {
   vpc_id = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-east-1a"
 }
-
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-2b"
+}
